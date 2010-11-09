0b61e46c6b8e02dbc7194c2efe9c235215616632
vweber <vincentweber@gmail.com>
2010-11-09 14:56:57 +0100

    Datastructure futher structered and all comments in English

--- lib/datastruc.hs
+++ lib/datastruc.hs
@@ -1,30 +1,61 @@
 {-
-De typeparameter a kan je gebruiken om extra informatie aan datastructuur toe te voegen.
+The Datastructure of the Hardware Visualisation project in Haskell. 
 
-Je zou bijvoorbeeld een functie kunnen maken die bepaald wat waar getekend moet worden.
-plaatsElementen :: ArchElem a -> ArchElem Positie
+Hardware will be specified by this structure, and will be interpreted by the visualisation program. The datastructure has recursive elmenents so function with deeper levels can be constructed too.
 
-en dan een teken functie die een argument van het type ArchElem Positie verwacht.
+To help the visualisation group we added an extra object a to make it possible to specify options, of other usefull extra information.
+For example a could be used to add information about the positioning of an object in gui. In this case a would be a Postion item (which is not specified in this document).
 
+Specified hardware by this structure always have a Function as the top node.
 
 -}
 data ArchElem a =
-    Function Id (Maybe Name) [InPort] OutPort ([ArchElem a],[Wire a]) a |
-    Operator Id OpType [In] Out a |
-    Literal Id Value Out a |
+    Function 
+        Id 
+        (Maybe Name)            -- ^ Optional name of the function
+        [InPort]                -- ^ List of the inports
+        OutPort                 -- ^ Outport of the function
+        ([ArchElem a],[Wire a]) -- ^ Functions have deeper architecture. These are specified by a tuple of a list of architectures and a list of wires, which connect the different architectures.
+        a |
+
+    Operator 
+        Id 
+        OpType 
+        [In] 
+        Out 
+        a |
+
+    Literal 
+        Id 
+        Value 
+        Out 
+        a |
 
     Mux Id   
-       [In] -- ^ de ingangen
-        Out -- ^ de uitgang
-        In  -- ^ de select ingang, die selecteert welke ingang er doorgaat naar de uitgang
+       [In] -- ^ List of inports from which the outport is selected
+        Out -- ^ The outport
+        In  -- ^ Inport signal to choise an from the list inports
         a |
     
-    Register Id (Maybe In) Out a |
-    PortReference Port
+    Register 
+        Id 
+        (Maybe In) 
+        Out 
+        a |
+
+    PortReference 
+        Port
+
     deriving (Show,Eq,Ord)
 
-data Wire a = Wire (Maybe Name) PortId PortId a     deriving (Show,Eq,Ord)
---                     naam     source dest
+data Wire a = 
+    Wire 
+        (Maybe Name)  -- ^ Optional name of the wire
+        PortId        -- ^ Source port
+        PortId        -- ^ Destination port
+        a     
+    deriving (Show,Eq,Ord)
+
 
 type Name = String
 type Value = String
@@ -38,7 +69,16 @@ type Out = Port
 
 type InPort = Port
 type OutPort = Port
-data Port = SinglePort PortId | MultiPort PortId [Port]     deriving (Show,Eq,Ord)
+
+data Port = 
+    SinglePort
+        PortId | 
+
+    MultiPort 
+        PortId 
+        [Port]     
+    
+    deriving (Show,Eq,Ord)
 {-
 Als een out(port) gewoon 1 enkele waarde teruggeeft,
 dan is het een 'SinglePort portId'
