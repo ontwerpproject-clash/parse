6478f394b8a88e9e05b43e48fa0aea13fcc2fd59
Leon Schoorl <l.m.schoorl@student.utwente.nl>
2010-12-08 13:21:06 +0100

    ParseClash: exported type PortId, OpType and Value from Datastruct

--- ParseClash.hs
+++ ParseClash.hs
@@ -1,6 +1,16 @@
 -- | This module is an external interface to Clash and our parsing of its output.
 --   If you want to use our code you should only have to import this module and nothing else.
-module ParseClash(parseClashFile,parseClashFiles, ArchElem(..),Wire(..),Port(..),Name,Id,In,Out,InPort,OutPort) where
+module ParseClash
+(
+parseClashFile, parseClashFiles,
+ArchElem(..),
+Wire(..),
+Port(..),
+Name,Id,PortId,
+In,Out,
+InPort,OutPort,
+OpType,Value
+) where
 
 -- local imports
 import Datastruct
