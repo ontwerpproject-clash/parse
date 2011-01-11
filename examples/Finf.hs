e7c1b71b79c9ceabfb4cdef299016cb60cb47368
mWaanders <m.j.c.waanders@student.utwente.nl>
2011-01-11 16:03:36 +0100

    Finf aangepast

--- examples/Finf.hs
+++ examples/Finf.hs
@@ -7,7 +7,9 @@ type Word = Signed D8
 
 {-# ANN finf TopEntity #-}
 finf :: Word -> Word -> State Word -> (State Word, Word)
-finf x y state = telop (x+5) y state
+finf x y state = (newstate,res+1)
+	where
+		(newstate,res) = telop (x+5) y state
 
 telop :: Word -> Word -> State Word -> (State Word, Word)
 telop x y state = (state, (x+y))
